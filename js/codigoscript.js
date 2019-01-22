165b7125c83ec645b54210773fc08802c1ea5bb1
PolRucana <hillsong123456789@gmail.com>
2019-01-22 00:15:59 -0500

    Se ha agregado el chat

--- js/codigoscript.js
+++ js/codigoscript.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $('.menu').click(function(){
         $('nav').toggleClass('aparicion');
+    });
+
+    $('.chat-button').on('click', function(e){
+        e.preventDefault();
+        $('.chat-content').slideToggle('fast');
+    });
+});
+
+/*$(document).on('ready',function(){
+    $('.chat-button').on('click', function(e){
+        e.preventDefault();
+        $('.chat-content').slideToggle('fast');
     })
-})
+});*/
